d0430fc5a6a8b3575698aa1780d5edf1fd3daecf
eelmadawy399-cmyk <eelmadawy399@gmail.com>
2026-02-19 17:08:32 +0200

    config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+// SAGE Egypt - Configuration
+var CONFIG = {
+    CLIENT_ID: '630643291056-u687ibvpr8oqpaalbo3qjig3r1659bap.apps.googleusercontent.com',
+    PROJECT_ID: 'ee-eelmadawy399',
+    SCOPES: [
+        'https://www.googleapis.com/auth/earthengine',
+        'https://www.googleapis.com/auth/devstorage.read_only'
+    ],
+    MAP_CENTER: [26.8, 30.8],  // Egypt center [lat, lng]
+    MAP_ZOOM: 6,
+    DEFAULT_BUFFER: 500  // meters
+};
